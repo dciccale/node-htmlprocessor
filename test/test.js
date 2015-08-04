cc310eb828dd7af4c24701c8092893c69892e523
Denis Ciccale <dciccale@gmail.com>
2015-08-04 12:32:35 +0200

    migrate tests to mocha and use istanbul for coverage

--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
 'use strict';
 
+var assert = require('assert');
 var utils = require('../lib/utils');
 var htmlprocessor = require('../index');
 
-module.exports = {
-  dev: function (test) {
-    test.expect(1);
-
+describe('dev', function () {
+  it('should process and output an html file as defined by the build special comments for dev target', function (done) {
     htmlprocessor({
         src: ['test/fixtures/index.html'],
         dest: 'test/fixtures/dev/index.processed.html'
@@ -21,14 +20,13 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/dev/index.processed.html');
     var expected = utils.read('test/expected/dev/index.html');
-    test.equal(actual, expected, 'should process and output an html file as defined by the build special comments for dev target');
+    assert.equal(actual, expected);
+    done();
+  });
+});
 
-    test.done();
-  },
-
-  dist: function (test) {
-    test.expect(1);
-
+describe('dist', function () {
+  it('should process and output an html file as defined by the build special comments for dist target', function (done) {
     htmlprocessor({
         src: ['test/fixtures/index.html'],
         dest: 'test/fixtures/dist/index.processed.html'
@@ -43,14 +41,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/dist/index.processed.html');
     var expected = utils.read('test/expected/dist/index.html');
-    test.equal(actual, expected, 'should process and output an html file as defined by the build special comments for dist target');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  custom: function (test) {
-    test.expect(1);
-
+describe('custom', function () {
+  it('should be able to process a template with custom delimiters', function (done) {
     htmlprocessor({
         src: ['test/fixtures/custom.html'],
         dest: 'test/fixtures/custom/custom.processed.html'
@@ -68,14 +66,15 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/custom/custom.processed.html');
     var expected = utils.read('test/expected/custom/custom.html');
-    test.equal(actual, expected, 'should be able to process a template with custom delimiters');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  marker: function (test) {
-    test.expect(1);
+describe('marker', function () {
 
+  it('should process and output an html file as defined by the build special comments for marker target', function (done) {
     htmlprocessor({
         src: ['test/fixtures/commentMarker.html'],
         dest: 'test/fixtures/commentMarker/commentMarker.processed.html'
@@ -91,14 +90,15 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/commentMarker/commentMarker.processed.html');
     var expected = utils.read('test/expected/commentMarker/commentMarker.html');
-    test.equal(actual, expected, 'should process and output an html file as defined by the build special comments for marker target');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  strip: function (test) {
-    test.expect(1);
+describe('strip', function () {
 
+  it('should remove build comments for non-targets', function (done) {
     htmlprocessor({
         src: ['test/fixtures/strip.html'],
         dest: 'test/fixtures/strip/strip.processed.html'
@@ -108,14 +108,15 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/strip/strip.processed.html');
     var expected = utils.read('test/expected/strip/strip.html');
-    test.equal(actual, expected, 'should remove build comments for non-targets');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  multiple: function (test) {
-    test.expect(3);
+describe('multiple', function () {
 
+  it('parse comment block defining multiple targets (1)', function (done) {
     htmlprocessor({
       src: ['test/fixtures/multiple.html'],
       dest: 'test/fixtures/multiple/mult_one.processed.html'
@@ -123,6 +124,13 @@ module.exports = {
         environment: 'mult_one'
     });
 
+    var actual = utils.read('test/fixtures/multiple/mult_one.processed.html');
+    var expected = utils.read('test/expected/multiple/mult_one.html');
+    assert.equal(actual, expected, 'parse comment block defining multiple targets (1)');
+    done();
+  });
+
+  it('parse comment block defining multiple targets (2)', function (done) {
     htmlprocessor({
       src: ['test/fixtures/multiple.html'],
       dest: 'test/fixtures/multiple/mult_two.processed.html'
@@ -130,6 +138,14 @@ module.exports = {
         environment: 'mult_two'
     });
 
+    var actual = utils.read('test/fixtures/multiple/mult_two.processed.html');
+    var expected = utils.read('test/expected/multiple/mult_two.html');
+    assert.equal(actual, expected);
+
+    done();
+  });
+
+  it('parse comment block defining multiple targets (3)', function (done) {
     htmlprocessor({
       src: ['test/fixtures/multiple.html'],
       dest: 'test/fixtures/multiple/mult_three.processed.html'
@@ -137,24 +153,16 @@ module.exports = {
         environment: 'mult_three'
     });
 
-    var actual = utils.read('test/fixtures/multiple/mult_one.processed.html');
-    var expected = utils.read('test/expected/multiple/mult_one.html');
-    test.equal(actual, expected, 'parse comment block defining multiple targets (1)');
+    var actual = utils.read('test/fixtures/multiple/mult_three.processed.html');
+    var expected = utils.read('test/expected/multiple/mult_three.html');
+    assert.equal(actual, expected);
 
-    actual = utils.read('test/fixtures/multiple/mult_two.processed.html');
-    expected = utils.read('test/expected/multiple/mult_two.html');
-    test.equal(actual, expected, 'parse comment block defining multiple targets (2)');
+    done();
+  });
+});
 
-    actual = utils.read('test/fixtures/multiple/mult_three.processed.html');
-    expected = utils.read('test/expected/multiple/mult_three.html');
-    test.equal(actual, expected, 'parse comment block defining multiple targets (3)');
-
-    test.done();
-  },
-
-  include_js: function (test) {
-    test.expect(1);
-
+describe('include_js', function () {
+  it('include a js file', function (done) {
     htmlprocessor({
       src: ['test/fixtures/include.html'],
       dest: 'test/fixtures/include/include.processed.html'
@@ -162,14 +170,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/include/include.processed.html');
     var expected = utils.read('test/expected/include/include.html');
-    test.equal(actual, expected, 'include a js file');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  conditional_ie: function (test) {
-    test.expect(1);
-
+describe('conditional_ie', function () {
+  it('correctly parse build comments inside conditional ie statement', function (done) {
     htmlprocessor({
       src: ['test/fixtures/conditional_ie.html'],
       dest: 'test/fixtures/conditional_ie/conditional_ie.processed.html'
@@ -177,14 +185,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/conditional_ie/conditional_ie.processed.html');
     var expected = utils.read('test/expected/conditional_ie/conditional_ie.html');
-    test.equal(actual, expected, 'correctly parse build comments inside conditional ie statement');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  recursive_process: function (test) {
-    test.expect(1);
-
+describe('recursive_process', function () {
+  it('recursively process included files', function (done) {
     htmlprocessor({
       src: ['test/fixtures/recursive.html'],
       dest: 'test/fixtures/recursive/recursive.processed.html'
@@ -194,14 +202,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/recursive/recursive.processed.html');
     var expected = utils.read('test/expected/recursive/recursive.html');
-    test.equal(actual, expected, 'recursively process included files');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  custom_blocktype: function (test) {
-    test.expect(1);
-
+describe('custom_blocktype', function () {
+  it('define custom block types', function (done) {
     htmlprocessor({
       src: ['test/fixtures/custom_blocktype.html'],
       dest: 'test/fixtures/custom_blocktype/custom_blocktype.processed.html'
@@ -211,14 +219,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/custom_blocktype/custom_blocktype.processed.html');
     var expected = utils.read('test/expected/custom_blocktype/custom_blocktype.html');
-    test.equal(actual, expected, 'define custom block types');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  attr: function (test) {
-    test.expect(1);
-
+describe('attr', function () {
+  it('modify attributes', function (done) {
     htmlprocessor({
       src: ['test/fixtures/attr.html'],
       dest: 'test/fixtures/attr/attr.processed.html'
@@ -226,14 +234,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/attr/attr.processed.html');
     var expected = utils.read('test/expected/attr/attr.html');
-    test.equal(actual, expected, 'modify attributes');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  template: function (test) {
-    test.expect(1);
-
+describe('template', function () {
+  it('replace template data', function (done) {
     htmlprocessor({
       src: ['test/fixtures/template.html'],
       dest: 'test/fixtures/template/template.processed.html'
@@ -246,14 +254,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/template/template.processed.html');
     var expected = utils.read('test/expected/template/template.html');
-    test.equal(actual, expected, 'replace template data');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  remove_no_newline: function (test) {
-    test.expect(1);
-
+describe('remove_no_newline', function () {
+  it('should remove block even if no starting new line is present', function (done) {
     htmlprocessor({
         src: ['test/fixtures/remove_no_newline.html'],
         dest: 'test/fixtures/remove/remove_no_newline.processed.html'
@@ -263,14 +271,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/remove/remove_no_newline.processed.html');
     var expected = utils.read('test/expected/remove/remove_no_newline.html');
-    test.equal(actual, expected, 'should remove block even if no starting new line is present');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  remove_with_newline: function (test) {
-    test.expect(1);
-
+describe('remove_with_newline', function () {
+  it('should remove block also when has a preceding newline', function (done) {
     htmlprocessor({
         src: ['test/fixtures/remove_with_newline.html'],
         dest: 'test/fixtures/remove/remove_with_newline.processed.html'
@@ -280,14 +288,14 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/remove/remove_with_newline.processed.html');
     var expected = utils.read('test/expected/remove/remove_with_newline.html');
-    test.equal(actual, expected, 'should remove block also when has a preceding newline');
+    assert.equal(actual, expected);
 
-    test.done();
-  },
+    done();
+  });
+});
 
-  inline: function (test) {
-    test.expect(1);
-
+describe('inline', function () {
+  it('should inline css and js for dist target', function (done) {
     htmlprocessor({
         src: ['test/fixtures/inline.html'],
         dest: 'test/fixtures/inline/inline.processed.html'
@@ -295,8 +303,8 @@ module.exports = {
 
     var actual = utils.read('test/fixtures/inline/inline.processed.html');
     var expected = utils.read('test/expected/inline/inline.html');
-    test.equal(actual, expected, 'should inline css and js for dist target');
+    assert.equal(actual, expected);
 
-    test.done();
-  }
-};
+    done();
+  });
+});
